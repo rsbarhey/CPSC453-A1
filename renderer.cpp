56fc67cf2106c6b802caa0fb23685349035cb0f9
rsbarhey <rsbarhey@gfx02-sa.cs.ucalgary.ca>
2015-09-23 16:22:20 -0600

    Initial Commit

---
+++ renderer.cpp
@@ -0,0 +1,187 @@
+#include "renderer.h"
+#include <QTextStream>
+#include <QOpenGLBuffer>
+#include <cmath>
+
+// constructor
+Renderer::Renderer(QWidget *parent)
+    : QOpenGLWidget(parent)
+{
+
+}
+
+// constructor
+Renderer::~Renderer()
+{
+
+}
+
+// called once by Qt GUI system, to allow initialization for OpenGL requirements
+void Renderer::initializeGL()
+{
+    // Qt support for inline GL function calls
+	initializeOpenGLFunctions();
+
+    // sets the background clour
+    glClearColor(0.7f, 0.7f, 1.0f, 1.0f);
+
+    // links to and compiles the shaders, used for drawing simple objects
+    m_program = new QOpenGLShaderProgram(this);
+    m_program->addShaderFromSourceFile(QOpenGLShader::Vertex, "per-fragment-phong.vs.glsl");
+    m_program->addShaderFromSourceFile(QOpenGLShader::Fragment, "per-fragment-phong.fs.glsl");
+    m_program->link();
+    m_posAttr = m_program->attributeLocation("position_attr");
+    m_colAttr = m_program->attributeLocation("colour_attr");
+    m_norAttr = m_program->attributeLocation("normal_attr");
+    m_PMatrixUniform = m_program->uniformLocation("proj_matrix");
+    m_VMatrixUniform = m_program->uniformLocation("view_matrix");
+    m_MMatrixUniform = m_program->uniformLocation("model_matrix");
+    m_programID = m_program->programId();
+
+}
+
+// called by the Qt GUI system, to allow OpenGL drawing commands
+void Renderer::paintGL()
+{
+    // Clear the screen buffers
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Set the current shader program
+
+    glUseProgram(m_programID);
+
+    // Modify the current projection matrix so that we move the
+    // camera away from the origin.  We'll draw the game at the
+    // origin, and we need to back up to see it.
+
+    QMatrix4x4 view_matrix;
+    view_matrix.translate(0.0f, 0.0f, -40.0f);
+    glUniformMatrix4fv(m_VMatrixUniform, 1, false, view_matrix.data());
+
+    // Not implemented: set up lighting (if necessary)
+
+    // Not implemented: scale and rotate the scene
+
+    QMatrix4x4 model_matrix;
+
+    // You'll be drawing unit cubes, so the game will have width
+    // 10 and height 24 (game = 20, stripe = 4).  Let's translate
+    // the game so that we can draw it starting at (0,0) but have
+    // it appear centered in the window.
+
+    model_matrix.translate(-5.0f, -12.0f, 0.0f);
+    glUniformMatrix4fv(m_MMatrixUniform, 1, false, model_matrix.data());
+
+    // Not implemented: actually draw the current game state.
+    // Here's some test code that draws red triangles at the
+    // corners of the game board.
+
+    generateBorderTriangles();
+
+    // draw border
+    if (triVertices.size() > 0)
+    {
+        // pass in the list of vertices and their associated colours
+        // 3 coordinates per vertex, or per colour
+        glVertexAttribPointer(m_posAttr, 3, GL_FLOAT, GL_FALSE, 0, &triVertices[0]);
+        glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, &triColours[0]);
+        glVertexAttribPointer(m_norAttr, 3, GL_FLOAT, GL_FALSE, 0, &triNormals[0]);
+
+        glEnableVertexAttribArray(m_posAttr);
+        glEnableVertexAttribArray(m_colAttr);
+        glEnableVertexAttribArray(m_norAttr);
+
+        // draw triangles
+        glDrawArrays(GL_TRIANGLES, 0, triVertices.size()/3); // 3 coordinates per vertex
+
+        glDisableVertexAttribArray(m_norAttr);
+        glDisableVertexAttribArray(m_colAttr);
+        glDisableVertexAttribArray(m_posAttr);
+    }
+
+    // deactivate the program
+    m_program->release();
+
+}
+
+// called by the Qt GUI system, to allow OpenGL to respond to widget resizing
+void Renderer::resizeGL(int w, int h)
+{
+    // width and height are better variables to use
+    Q_UNUSED(w); Q_UNUSED(h);
+
+    // update viewing projections
+    glUseProgram(m_programID);
+
+    // Set up perspective projection, using current size and aspect
+    // ratio of display
+    QMatrix4x4 projection_matrix;
+    projection_matrix.perspective(40.0f, (GLfloat)width() / (GLfloat)height(),
+                                  0.1f, 1000.0f);
+    glUniformMatrix4fv(m_PMatrixUniform, 1, false, projection_matrix.data());
+
+    glViewport(0, 0, width(), height());
+
+}
+
+// computes the vertices and corresponding colours-per-vertex for a quadrilateral
+// drawn from (x1, y1) to (x2, y2)
+// Note: the magic numbers in the vector insert commands should be better documented
+void Renderer::generateBorderTriangles()
+{
+    // make sure array lists are clear to start with
+    triVertices.clear();
+    triColours.clear();
+
+    // add vertices to rectangle list
+    float vectList [] = {
+        0.0, 0.0, 0.0,  // bottom left triangle
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+
+        9.0, 0.0, 0.0,  // bottom right triangle
+        10.0, 0.0, 0.0,
+        10.0, 1.0, 0.0,
+
+        0.0, 19.0, 0.0, // top left triangle
+        1.0, 20.0, 0.0,
+        0.0, 20.0, 0.0,
+
+        10.0, 19.0, 0.0,    // top right triangle
+        10.0, 20.0, 0.0,
+        9.0, 20.0, 0.0 };
+    triVertices.insert(triVertices.end(), vectList, vectList + 3*4*3); // 36 items in array
+
+    // shader supports per-vertex colour; add colour for each vertex add colours to colour list - use current colour
+    QColor borderColour = Qt::red;
+    float colourList [] = { (float)borderColour.redF(), (float)borderColour.greenF(), (float)borderColour.blueF() };
+    float normalList [] = { 0.0f, 0.0f, 1.0f }; // facing viewer
+    for (int v = 0; v < 4 * 3; v++)
+    {
+        triColours.insert(triColours.end(), colourList, colourList + 3); // 3 coordinates per vertex
+        triNormals.insert(triNormals.end(), normalList, normalList + 3); // 3 coordinates per vertex
+    }
+
+}
+
+// override mouse press event
+void Renderer::mousePressEvent(QMouseEvent * event)
+{
+    QTextStream cout(stdout);
+    cout << "Stub: Button " << event->button() << " pressed.\n";
+}
+
+// override mouse release event
+void Renderer::mouseReleaseEvent(QMouseEvent * event)
+{
+    QTextStream cout(stdout);
+    cout << "Stub: Button " << event->button() << " pressed.\n";
+}
+
+// override mouse move event
+void Renderer::mouseMoveEvent(QMouseEvent * event)
+{
+    QTextStream cout(stdout);
+    cout << "Stub: Motion at " << event->x() << ", " << event->y() << ".\n";
+}
---
+++ renderer.h
@@ -0,0 +1,84 @@
+/*
+ * CPSC 453 - Introduction to Computer Graphics
+ * Assignment 1
+ *
+ * Renderer - OpenGL widget for drawing scene
+ */
+
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QMatrix4x4>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLShader>
+#include <QMouseEvent>
+
+using namespace std;
+
+class Renderer : public QOpenGLWidget, protected QOpenGLFunctions
+{
+
+    // informs the qmake that a Qt moc_* file will need to be generated
+    Q_OBJECT
+
+public:
+    // constructor
+    Renderer(QWidget *parent = 0);
+
+    // destructor
+    virtual ~Renderer();
+
+protected:
+
+    // override fundamental drawing functions
+
+    // Called when OpenGL is first initialized
+    void initializeGL();
+
+    // Called when the window is resized
+    void resizeGL(int w, int h);
+
+    // Called when the window needs to be redrawn
+    void paintGL();
+
+    // override mouse event functions
+
+    // Called when a mouse button is pressed
+    virtual void mousePressEvent(QMouseEvent * event);
+
+    // Called when a mouse button is released
+    virtual void mouseReleaseEvent(QMouseEvent * event);
+
+    // Called when the mouse moves
+    virtual void mouseMoveEvent(QMouseEvent * event);
+
+private:
+
+    // member variables for shader manipulation
+    GLuint m_programID;
+    GLuint m_posAttr;
+    GLuint m_colAttr;
+    GLuint m_norAttr;
+    GLuint m_MMatrixUniform; // model matrix
+    GLuint m_VMatrixUniform; // view matrix
+    GLuint m_PMatrixUniform; // projection matrix
+
+    QOpenGLShaderProgram *m_program;
+
+    // for storing triangle vertices and colours
+    vector<GLfloat> triVertices;
+    vector<GLfloat> triColours;
+    vector<GLfloat> triNormals;
+
+    // helper function for loading shaders
+    GLuint loadShader(GLenum type, const char *source);
+
+    // helper function for drawing bordering triangles
+    void generateBorderTriangles();
+
+};
+
+#endif // RENDERER_H
